800f0da8cf444935b7bbb51993bd2a29ee411f23
Juan Melo <jmelopdev@gmail.com>
2024-09-19 22:00:09 +0100

    feat: change token urls and increase token expiration time for improved authentication

--- LittleLemonApp/urls.py
+++ LittleLemonApp/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MenuItemListView, UserView
+from .views import MenuItemView, UserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # API users
     path('api/users/users/me/', UserView.as_view(), name='users_users_me'),
     path('api/users', UserView.as_view(), name='users_users_me'),
-    path('token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # API menu items
-    path('api/menu-items/', MenuItemListView.as_view(), name='menu_item_list'),
+    path('api/menu-items', MenuItemView.as_view(), name='menu_item_list'),
 ]
