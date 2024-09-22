cbc8514653c447b66a8c6c6615f4d4cf50bb833f
Juan Melo <jmelopdev@gmail.com>
2024-09-22 13:41:13 +0100

    feat: new endpoint to add users to manager group

--- LittleLemonApp/urls.py
+++ LittleLemonApp/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('api/menu-items/<int:pk>', MenuItemDetailView.as_view(), name='menu_item_list'),
     # API groups manager users
     path('api/groups/manager/users', ManagerListView.as_view(), name='manager_list_view'),
+    path('api/groups/manager/users/<int:user_id>/', ManagerListView.as_view(), name='manager_list_view'),
     path('api/groups/delivery-crew/users', DeliveryCrewListView.as_view(), name='delivery-crew_list_view'),
 ]
--- LittleLemonApp/views.py
+++ LittleLemonApp/views.py
@@ -95,6 +95,40 @@ class ManagerListView(APIView):
             
             return Response(managers_data, status=200)
         return Response({'detail': 'Permission denied.'}, status=status.HTTP_403_FORBIDDEN)
+    
+    def post(self, request, *args, **kwargs):
+        if IsManager().has_permission(request, self):
+            try:
+                manager_group = Group.objects.get(name="Manager")
+            except Group.DoesNotExist:
+                return Response({"detail": "Manager group not found."}, status=404)
+            
+            user_id = request.data.get('user_id')
+            if not user_id:
+                return Response({"detail": "User ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                user = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+            
+            user.groups.add(manager_group)
+            return Response({"detail": f"User {user.username} added to Manager group."}, status=status.HTTP_201_CREATED)
+        
+    def delete(self, request, user_id, *args, **kwargs):
+        if IsManager().has_permission(request, self):
+            try:
+                manager_group = Group.objects.get(name="Manager")
+            except Group.DoesNotExist:
+                return Response({"detail": "Manager group not found."}, status=404)
+
+            try:
+                user = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+            
+            user.groups.remove(manager_group)
+            return Response({"detail": f"User {user.username} removed to Manager group."}, status=status.HTTP_201_CREATED)
 
 class DeliveryCrewListView(APIView):
     permission_classes = [IsAuthenticated]
