a02adc188ed3efc94c53357c3e9efe9a5dbdfe0e
Juan Melo <jmelopdev@gmail.com>
2024-09-22 22:24:17 +0100

    feat: add new cart models

--- LittleLemonApp/models.py
+++ LittleLemonApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=35)
@@ -7,3 +8,18 @@ class MenuItem(models.Model):
     
     def __str__(self):
         return self.name
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.menu_item.name}"
