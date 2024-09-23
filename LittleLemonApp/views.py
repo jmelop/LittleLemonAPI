32dd9711e47965a6791462eb84318f5bf0558b00
Juan Melo <jmelopdev@gmail.com>
2024-09-24 00:06:17 +0100

    feat: implemented filtering, pagination and sorting to get orders endpoint

--- LittleLemonApp/views.py
+++ LittleLemonApp/views.py
@@ -218,19 +218,30 @@ class CartMenuItemsView(generics.ListAPIView):
             return Response({'detail': 'No items found in the cart.'}, status=status.HTTP_404_NOT_FOUND)
 
 class OrderMenuItemsView(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = Order.objects.all()
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['status', 'delivery_crew', 'created_at']
+    ordering_fields = ['created_at', 'status', 'user']
+    ordering = ['created_at']
+    
+    def get_queryset(self):
+        return Order.objects.all()
 
     def get(self, request, *args, **kwargs):
         if IsManager().has_permission(request, self):
-            orders = Order.objects.all()
-            serializer = OrderSerializer(orders, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            queryset = self.get_queryset()
         elif IsCustomerOrDeliveryCrew().has_permission(request, self):
-                orders = Order.objects.filter(user=request.user)
-                serializer = OrderItemSerializer(orders, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({"detail": "Permission denied."}, status=status.HTTP_403_FORBIDDEN)
+            queryset = self.get_queryset().filter(user=request.user)
+        else:
+            return Response({"detail": "Permission denied."}, status=status.HTTP_403_FORBIDDEN)
+        filtered_queryset = self.filter_queryset(queryset)
+        page = self.paginate_queryset(filtered_queryset)
+        if page is not None:
+            serializer = OrderSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = OrderSerializer(filtered_queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request, *args, **kwargs):
         if IsCustomer().has_permission(request, self):
