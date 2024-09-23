0786f55a8d5df3e19665130da492b6f5dac4a8b7
Juan Melo <jmelopdev@gmail.com>
2024-09-24 00:04:46 +0100

    feat: add django filter library

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -40,12 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'LittleLemonApp',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'django_filters'
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
     ),
 }
 
