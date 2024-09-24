b7ade466bb9449e545db732080b388530885673d
Juan Melo <jmelopdev@gmail.com>
2024-09-24 09:42:12 +0100

    fix: change url to login or refresh the token

--- LittleLemonApp/urls.py
+++ LittleLemonApp/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     # API users
     path('api/users/users/me/', UserView.as_view(), name='users_users_me'),
     path('api/users', UserView.as_view(), name='users_users_me'),
-    path('api/token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # API categories
+    path('api/categories', MenuItemView.as_view(), name='menu_item_list'),
     # API menu items
     path('api/menu-items', MenuItemView.as_view(), name='menu_item_list'),
     path('api/menu-items/<int:pk>', MenuItemDetailView.as_view(), name='menu_item_list'),
